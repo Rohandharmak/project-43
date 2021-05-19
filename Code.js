2d9662065dec5ae50ec325b3301b120d45911a9a
Rohandharmak <71077549+Rohandharmak@users.noreply.github.com>
2021-05-19 20:23:26 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+        fill("white")
+        textSize(15)
+        text("R E V B A I L A", 100,50)
+        fill("lightblue")
+        text("Hint: Always changing, not constant !!", 100,70)
+
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 300,50)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 500,50)
+    fill("lightblue")
+    text("Stores all information !!", 100,70)
+        
+
+    }
